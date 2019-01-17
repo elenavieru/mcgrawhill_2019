7d97d2b8236f6c6d24d44791a569b0708b338886
Elena Vieru <33833374+elenavieru@users.noreply.github.com>
2019-01-17 12:22:56 +0100

    Create style.js

---
+++ style.js
@@ -0,0 +1,497 @@
+(function (blink) {
+	'use strict';
+
+	var McGrawHillStyle = function () {
+			blink.theme.styles.basic.apply(this, arguments);
+		},
+		page = blink.currentPage;
+
+	McGrawHillStyle.prototype = {
+		bodyClassName: 'content_type_clase_mcgrawhill',
+		toolbar: { name: 'editorial', items: ['Blink_popover'] },
+		extraPlugins: ['blink_popover'],
+		ckEditorStyles: {
+			name: 'mcgrawhill',
+			styles: [
+				{ name: 'Título 1', element: 'h2', attributes: { 'class': 'bck-title bck-title-1'} },
+				{ name: 'Título 2', element: 'h3', attributes: { 'class': 'bck-title bck-title-2'} },
+				{ name: 'Título 3', element: 'h3', attributes: { 'class': 'bck-title bck-title-3'} },
+
+				{ name: 'Énfasis', element: 'span', attributes: { 'class': 'bck-enfasis'} },
+
+				{ name: 'Lista Desordenada', element: 'ul', attributes: { 'class': 'bck-ul'} },
+				{ name: 'Lista Ordenada 1', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-1' } },
+				{ name: 'Lista Ordenada 2', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-2' } },
+				{ name: 'Lista Ordenada 3', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-3' } },
+
+				{ name: 'Caja 1', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-1' } },
+				{ name: 'Caja 2', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-2' } },
+
+				{ name: 'Tabla', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table'} },
+				{ name: 'Celda 1', element: 'td', attributes: { 'class': 'bck-td bck-td-1'} },
+				{ name: 'Celda 2', element: 'td', attributes: { 'class': 'bck-td bck-td-2'} },
+				{ name: 'Celda 3', element: 'td', attributes: { 'class': 'bck-td bck-td-3'} },
+				{ name: 'Celda 4', element: 'td', attributes: { 'class': 'bck-td bck-td-4'} },
+
+				{ name: 'Enlace web', element: 'a', attributes: { 'class': 'bck-link'} },
+
+				{ name: 'Desplegable 1', type: 'widget', widget: 'blink_dropdown', attributes: { 'class': 'bck-dropdown bck-dropdown-1' } },
+				{ name: 'Desplegable 2', type: 'widget', widget: 'blink_dropdown', attributes: { 'class': 'bck-dropdown bck-dropdown-2' } }
+			]
+		},
+		slidesTitle: {},
+		subunits: [],
+		totalSlides: 0,
+
+		init: function () {
+			var parent = blink.theme.styles.basic.prototype,
+				that = this;
+			that.activityDropdown();
+			parent.init.call(that);
+			that.addActivityTitle();
+			if(window.esWeb) return;
+			that.fillSlidesTitle();
+			that.getActualUnitActivities();
+			blink.events.on("course_loaded", function(){
+				that.formatCarouselindicators();
+				that.enableSliders();
+			});
+			that.animateNavbarOnScroll();
+			that.initDropdown();
+			parent.initInfoPopover();
+			that.addSlideNavigators();
+		},
+
+		removeFinalSlide: function () {
+			var parent = blink.theme.styles.basic.prototype;
+			parent.removeFinalSlide.call(this, true);
+		},
+
+		configEditor: function (editor) {
+			editor.dtd.$removeEmpty['span'] = false;
+		},
+
+		addActivityTitle: function () {
+			if (!blink.courseInfo || !blink.courseInfo.unit) return;
+			$('.libro-left').find('.title').html(function () {
+				return blink.courseInfo.unit + ' > ' + $(this).html();
+			})
+		},
+
+		fillSlidesTitle: function () {
+			var self = this.slidesTitle;
+			for (var index = 0; index < window.secuencia.length; index++) {
+				var slide = window['t'+index+'_slide'];
+				var slideTitle = slide.title;
+				slideTitle = slideTitle.replace(/<span class="index">\s*([\d]+)\s*<\/span>/i, '$1. ');
+				slideTitle = slideTitle.replace(/\s+/, ' ');
+				slideTitle = stripHTML(slideTitle);
+
+				self['t'+index+'_slide'] = slideTitle;
+			}
+		},
+
+		/**
+		 * @summary Gets the activity type subunits of the actual unit.
+		 * @return {Object} Object of the actual unit filtering the not activity type subunits
+		 */
+		getActualUnitActivities: function () {
+			var curso = blink.getCourse(idcurso),
+				that = this,
+				units,
+				unitSubunits,
+				actualActivity,
+				unitActivities = [];
+
+			curso.done(function () {
+				units = curso.responseJSON.units;
+
+				$.each(units, function () {
+					if (this.id && this.id == blink.courseInfo.IDUnit) {
+						unitSubunits = this.subunits.concat(this.resources);
+					}
+				});
+
+				actualActivity = _.find(unitSubunits, function(subunit) {
+					return subunit.id == idclase;
+				});
+
+				if (typeof actualActivity !== "undefined" && actualActivity.level == '6') {
+					unitActivities.push(actualActivity);
+				} else {
+					unitActivities = _.filter(unitSubunits, function(subunit) {
+						return subunit.type == 'actividad' && subunit.level !== '6';
+					});
+				}
+
+				that.subunits = unitActivities;
+			}).done(function(){
+				blink.events.trigger('course_loaded');
+			});
+		},
+
+		/**
+		 * @summary Getting active slide position in relation with the total of the
+		 *          unit slides.
+		 * @param {Array} $subunits Array of activity type objects
+		 * @return {int} Slide position
+		 */
+		getActualSlideNumber: function (subunits) {
+			var actualSlideIndex = $('.swipeview-active').attr('data-page-index'),
+				actualSlide = 1;
+
+			for (var i in subunits) {
+				if (subunits[i].id && parseInt(subunits[i].id) != idclase) {
+					actualSlide += parseInt(subunits[i].pags);
+				} else {
+					actualSlide += parseInt(actualSlideIndex);
+					break;
+				}
+			}
+
+			return actualSlide;
+		},
+
+		formatCarouselindicators: function () {
+			var that = this,
+				$navbarBottom = $('.navbar-bottom'),
+				firstSlide = eval('t0_slide');
+			if(blink.courseInfo && blink.courseInfo.courseDateCreated) var courseYearCreated = new Date(blink.courseInfo.courseDateCreated).getFullYear();
+			var yearCopy = courseYearCreated !== undefined ? courseYearCreated : 2016;
+			$navbarBottom
+				.attr('class', 'mcgrawhill-navbar')
+				.wrapInner('<div class="navbar-content"></div>')
+				.find('ol')
+					.before('<span class="copyright">&copy;' +  yearCopy + '</span>')
+					.wrap('<div id="top-navigator"/>')
+					.remove()
+					.end();
+
+			$('#volverAlIndice').click(function() {
+				return showCursoCommit();
+			});
+
+			var subunits = that.subunits,
+				totalSlides = 0,
+				subunit_index,
+				subunit_pags;
+
+			// Different behaviour depending on whether the slides are accessed from
+			// a book or from a homework link or similar
+			if (subunits.length !== 0) {
+				for (var i in subunits) {
+					if (subunits[i].pags) {
+						var subunitSlides = parseInt(subunits[i].pags);
+						totalSlides += subunitSlides;
+					}
+					if (subunits[i].id && subunits[i].id == idclase) {
+						subunit_index = i;
+						subunit_pags = parseInt(subunits[i].pags);
+					}
+				}
+
+				that.totalSlides = totalSlides;
+
+				$('#top-navigator').append('<span class="left slider-navigator">' +
+						'<span class="fa fa-chevron-left"></span>' +
+					'</span>' +
+					'<span class="slide-counter" data-subunit-index="' + subunit_index +
+						'" data-subunit-pags="' + subunit_pags + '">' +
+						that.getActualSlideNumber(subunits) + ' / ' + totalSlides +
+					'</span>' +
+					'<span class="right slider-navigator">' +
+						'<span class="fa fa-chevron-right"></span>' +
+					'</span>');
+
+				blink.events.on('section:shown', function() {
+					$('.slide-counter').html(that.getActualSlideNumber(subunits) +
+						' / ' + totalSlides);
+				});
+			} else {
+				$('#top-navigator').append('<span class="left slider-navigator">' +
+						'<span class="fa fa-chevron-left"></span>' +
+					'</span>' +
+					'<span class="slide-counter">' + (window.activeSlide + 1) +
+						' / ' + window.secuencia.length +
+					'</span>' +
+					'<span class="right slider-navigator">' +
+						'<span class="fa fa-chevron-right"></span>' +
+					'</span>');
+
+				blink.events.on('section:shown', function() {
+					$('.slide-counter').html((window.activeSlide + 1) +
+						' / ' + window.secuencia.length);
+					$('.bck-dropdown-2').hideBlink();
+				});
+			}
+
+			blink.events.on('section:shown', function() {
+				var sectionTitle = eval('t' + blink.activity.getFirstSlideIndex(window.activeSlide) +
+					'_slide').title;
+				$navbarBottom.find('.sectionTitle').text(sectionTitle);
+			});
+
+			if (firstSlide.seccion) {
+				$navbarBottom.addClass('first-is-section');
+			}
+
+			blink.events.trigger(true, 'style:endFormatCarousel');
+		},
+
+		addSlideNavigators: function () {
+			blink.events.on("course_loaded", function(){
+				var that = blink.activity.currentStyle,
+					subunit_index = parseInt($('.slide-counter').attr('data-subunit-index'));
+
+				$('.slider-control').off('click');
+
+				// Navigation change depending on whether the slides are accessed from
+				// a book or from a homework link or similar
+				if (that.subunits.length !== 0) {
+					// Slider controls must allow navigation among all the activity subunits
+					// in the current unit.
+					$('.left.slider-control, .left.slider-navigator').click(function () {
+						if (!$(this).hasClass('disabled')) {
+							if(activeSlide == 0) {
+								redireccionar('/coursePlayer/clases2.php?editar=0&idcurso=' +
+									idcurso + '&idclase=' + that.subunits[subunit_index - 1].id + '&modo=0&numSec=' +
+									that.subunits[subunit_index - 1].numSlides, false, undefined);
+							} else {
+								blink.activity.showPrevSection();
+						}
+						}
+					});
+					$('.right.slider-control, .right.slider-navigator').click(function () {
+						if (!$(this).hasClass('disabled')) {
+							if(activeSlide == parseInt(that.subunits[subunit_index].pags) - 1) {
+								redireccionar('/coursePlayer/clases2.php?editar=0&idcurso=' +
+									idcurso + '&idclase=' + that.subunits[subunit_index + 1].id + '&modo=0' + ((typeof window.esPopup !== "undefined" && window.esPopup)?"&popup=1":""),
+									false, undefined);
+							} else {
+								blink.activity.showNextSection();
+							}
+						}
+					});
+				} else {
+					$('.left.slider-control, .left.slider-navigator').click(function () {
+						blink.activity.showPrevSection();
+					});
+					$('.right.slider-control, .right.slider-navigator').click(function () {
+						blink.activity.showNextSection();
+					});
+				}
+
+				$(document).ready(function() {
+					blink.events.on('showSlide:after', function() {
+						that.enableSliders();
+					});
+				});
+			});
+		},
+
+		/**
+		 * @summary Enables all slider controls and disables when appropiate
+		 */
+		enableSliders: function () {
+			// Removes disabled class to all navigation buttons and applies
+			// just if its first or last slide of all activities
+			$('.slider-control, .slider-navigator').removeClass('disabled');
+			var that = blink.activity.currentStyle,
+					subunit_index = parseInt($('.slide-counter').attr('data-subunit-index'));
+
+			// Navigation change depending on whether the slides are accessed from
+			// a book or from a homework link or similar
+			if (this.subunits.length !== 0 && modoVisualizacionLabel != "standalone") {
+				if (this.getActualSlideNumber(this.subunits) == 1) {
+					$('.slider-control.left, .slider-navigator.left').addClass('disabled');
+				}
+				if (this.getActualSlideNumber(this.subunits) == this.totalSlides) {
+					$('.slider-control.right, .slider-navigator.right').addClass('disabled');
+				}
+			} else {
+				if (window.activeSlide == 0) {
+					$('.slider-control.left, .slider-navigator.left').addClass('disabled');
+				}
+				if(window.activeSlide == parseInt(that.subunits[subunit_index].pags) - 1){
+					$('.slider-control.right, .slider-navigator.right').addClass('disabled');
+				}
+			}
+		},
+
+		getEditorStyles: function () {
+			return this.ckEditorStyles;
+		},
+
+		showBookIndexInClass: function () {
+			return modoVisualizacionLabel != "standalone";
+
+		},
+
+		animateNavbarOnScroll: function () {
+			if (!blink.isApp) return;
+			var $navbar = $('.mcgrawhill-navbar');
+			var lastScrollTop = 0;
+			$('.js-slider-item').scroll(function () {
+				var scrollTop = $(this).scrollTop();
+				(scrollTop > lastScrollTop && scrollTop) ? $navbar.addClass('ocultar') : $navbar.removeClass('ocultar');
+				lastScrollTop = scrollTop;
+			});
+		},
+
+		/**
+		 * @summary Sets concatenate slides inside a dropdown with togglable tabs.
+		 */
+		activityDropdown: function () {
+			for(var index = 1; index < secuencia.length; index++) {
+				var slide = eval('t'+index+'_slide'),
+					prevSlide = eval('t' + (index - 1) + '_slide'),
+					not_exercise = [1, 16, 24];
+				// Checks if the slide is type exercise and concatenate
+				if (slide.isConcatenate && $.inArray(slide.tipo, not_exercise) == -1) {
+					var $slide = $('#t'+index+'pg'),
+						$slide_container = $slide.closest('.item-container'),
+						tab_index;
+					// Constructs the containing dropdown if it doesn't exists
+					if ($.inArray(prevSlide.tipo, not_exercise) != -1 || !prevSlide.isConcatenate) {
+						var dropdown =
+							'<div class="bck-dropdown bck-dropdown-2 activities-dropdown">' +
+								'<div class="bck-dropdown-button">' +
+									'<div class="bck-dropdown-icon">' +
+										'<div class="fa fa-chevron-down"></div>' +
+									'</div>' +
+									'<div class="bck-dropdown-titulo">' +
+										textweb('activities') +
+									'</div>' +
+								'</div>' +
+								'<div class="bck-dropdown-content">' +
+									// Nav-tabs
+  									'<ul class="nav nav-tabs" role="tablist"></ul>' +
+									// Tab-panes
+  									'<div class="tab-content"></div>' +
+								'</div>' +
+							'</div>';
+						var slide_main = $('#t'+(index-1)+'pg').closest('.js-slide-wrapper');
+						$(slide_main).append(dropdown);
+						tab_index = 1;
+					}
+					var dropdown_container = $slide_container.find('.activities-dropdown').last(),
+						nav_tabs = $(dropdown_container).find('.nav-tabs')[0],
+						tab_content = $(dropdown_container).find('.tab-content')[0],
+						$concatenate_slide;
+
+					// Creates a tab and a pane for each concatenate slide
+					$(nav_tabs).append('<li role="presentation">' +
+							'<a href="#activity-' + index + '" aria-controls="activity-' + index + '" role="tab" data-toggle="tab">' +
+								tab_index +
+							'</a>' +
+						'</li>');
+
+					$(tab_content).append('<div role="tabpanel" class="tab-pane" id="activity-' + index + '">' +
+						'</div>');
+
+					// Detaches the concatenate slide and appends it to its pane
+					$concatenate_slide = $slide.closest('.slide-concatenate').detach();
+					$concatenate_slide.appendTo('#activity-' + index);
+
+					// Sets the first activity to active
+					if (tab_index == 1) {
+						$('a[href="#activity-' + index + '"]').closest('li').addClass('active');
+						$('#activity-' + index).addClass('active');
+					}
+
+					// Gives each concatenate activity the box-2 style
+					$('#transp' + index).addClass('bck-box bck-box-2');
+					$('#transp' + index + ' > .header').addClass('bck-title js--title');
+					$('#transp' + index + ' > *:not(.header)').wrapAll('<div class="bck-content" />');
+
+					tab_index++;
+				}
+			}
+		},
+
+		initDropdown : function () {
+			$(".bck-dropdown-button")
+				.find('.fa').text('').end() //Eliminar el &nbsp;
+				.toggle(
+					function(){
+						$(this).parent('.bck-dropdown')
+							.addClass('open')
+							.find('.fa').removeClass('fa-chevron-down').addClass('fa-chevron-up').end()
+							.find(".bck-dropdown-content").slideDown(300, function() {
+								if($(this).parent('.bck-dropdown').hasClass('activities-dropdown')
+									&& blink.activity.currentStyle.getActivePane()) {
+									var actividad = $('.tab-pane.active').attr('id');
+									var index = actividad.split('-').pop();
+									var slide = window['t' + index + '_slide'];
+									slide.onAfterShowSlide();
+								}
+							});
+
+					},
+					function(){
+						$(this).parent('.bck-dropdown')
+							.removeClass('open')
+							.find('.fa').removeClass('fa-chevron-up').addClass('fa-chevron-down').end()
+							.find(".bck-dropdown-content").slideUp(300);
+					}
+				);
+		},
+
+		// Busca la id de la última slide de un dropdown con la que se ha interactuado y
+		// guarda en el suspend data.
+		// @param {string} activePane 	Id del panel en el que se encuentra la slide
+		setActivePane : function (activePane) {
+			if (!!activePane && !!scormAPI) {
+				var slideId = $(activePane).find('.class_slide').attr('data-id'),
+					parentSlide = window['t' + window.activeSlide + '_slide'];
+				parentSlide.sublocation = slideId;
+			}
+		},
+
+		// Mira a ver si se ha guardado alguna slide del dropdown en el suspend data y,
+		// en caso de ser así, la pone como activa.
+		getActivePane : function () {
+			var currentSlide = window['t' + window.activeSlide + '_slide'];
+			if (currentSlide.sublocation) {
+				var activePane = $('.class_slide[data-id="' + currentSlide.sublocation + '"]')
+								.closest('.tab-pane'),
+					activeDropdown = $(activePane).closest('.bck-dropdown');
+				$(activeDropdown)
+					.find('a[href="#' + $(activePane).attr('id') + '"]')
+					.first()
+					.tab('show');
+				return true;
+			} else {
+				return true;
+			}
+		}
+	};
+
+	McGrawHillStyle.prototype = _.extend({}, new blink.theme.styles.basic(), McGrawHillStyle.prototype);
+
+	blink.theme.styles['mcgrawhill'] = McGrawHillStyle;
+
+})( blink );
+
+$(document).ready(function() {
+
+	if (!$('body').hasClass('edit')) {
+		$(document).on('click', '.nav-tabs a', function() {
+			var actividad = $(this).attr('href');
+			var index = actividad.split('-').pop();
+			var slide = window['t' + index + '_slide'];
+			slide.onAfterShowSlide();
+		});
+	}
+
+	$(document).on('click', '.bck-dropdown .class_slide, .nav-tabs a', function(e){
+		var activePane;
+		if (!!$(e.target).closest('.nav-tabs').length) {
+			activePane = $(e.target).attr('href');
+		} else {
+			activePane = '#' + $(e.target).closest('.tab-pane').attr('id');
+		}
+		blink.activity.currentStyle.setActivePane(activePane);
+	});
+
+});
