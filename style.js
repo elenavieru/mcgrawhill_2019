d43800bbfe91bd03d1c89bf6f2957d80b0ed4a22
Elena Vieru <33833374+elenavieru@users.noreply.github.com>
2019-03-12 17:28:02 +0100

    Update style.js

--- style.js
+++ style.js
@@ -37,6 +37,8 @@
 				{ name: 'Caja Actividades', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-4' } },
 				{ name: 'Caja Colores', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-5' } },
 				{ name: 'Caja Inicio Degradado', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-6' } },
+				{ name: 'Caja Descubre', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-7' } },
+		
 
 				{ name: 'Tabla', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table'} },
 				{ name: 'Celda 1', element: 'td', attributes: { 'class': 'bck-td bck-td-1'} },
